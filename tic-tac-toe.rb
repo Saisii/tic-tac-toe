f4618665d7aaa07f9d55afdcb26933b195997d18
Saisii <sethconger@gmail.com>
2022-11-24 18:32:05 +0300

    player O and player X input function complete

--- tic-tac-toe.rb
+++ tic-tac-toe.rb
@@ -1,12 +1,31 @@
-p board = Array.new(9)
+p board = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
 
 def display_board (board) 
     puts " #{board[0]} | #{board[1]} | #{board[2]}"
-    puts "--------"
+    puts "----------"
     puts " #{board[3]} | #{board[4]} | #{board[5]}"
-    puts "--------"
+    puts "----------"
     puts " #{board[6]} | #{board[7]} | #{board[8]}"
 end
 
 display_board (board)
+
+def player_O_move (board)
+    puts "Where do you want to put an O?"
+    choice = gets.chomp.to_i - 1
+    # board[choice] = board[choice].to_s
+    board[choice] = "O" 
+    display_board (board)
+end
+
+def player_X_move (board)
+    puts "Where do you want to put an X?"
+    choice = gets.chomp.to_i - 1
+    board[choice] = "X"
+    display_board (board)
+end
+
+
+player_O_move (board)
+player_X_move (board)
 
